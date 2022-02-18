b4339386d64f52fd1adc302fa8655e6afb20a23e
Marco Aurélio Ghelere <cau.ghelere@hotmail.com>
2022-02-17 22:26:07 -0300

    Error alerts added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
-function mostrarErro(elementoFormulario, tipo, msg_erro){
-    const 
+function msgErro(){
+    var validarNome = document.forms["formulario"]["nome"].value;
+    var validarCPF = document.forms["formulario"]["cpf"].value;
+    var validarFone = document.forms["formulario"]["telefone"].value;
+    var validarEmail = document.forms["formulario"]["email"].value;
+
+    if(validarNome == ""){
+        alert("Campo NOME está vazio");
+        return false;
+    }
+    else if(isNaN(validarCPF)){
+        alert("CPF deve ser preenchido apenas com números");
+        return false;
+    }
+    else if(validarCPF == ""){
+        alert("Campo CPF está vazio");
+        return false;
+    }
+    else if(isNaN(validarFone)){
+        alert("Telefone deve ser preenchido apenas com números");
+        return false;
+    }
+    else if(validarFone == ""){
+        alert("Campo TELEFONE está vazio");
+        return false;
+    }
+    else if(validarEmail == ""){
+        alert("Campo EMAIL está vazio");
+        return false;
+    }
+    
 }
